c907435f5391e0b9d9d325698d856f73f1111b30
Zakary <119669322+zakaryjs@users.noreply.github.com>
2024-02-26 22:00:04 +0800

    feat: guessing game complete

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
-
     loop {
         println!("Please input your guess.");
 
@@ -16,14 +14,20 @@ fn main() {
 
         io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 
